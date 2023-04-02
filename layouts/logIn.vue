b5646e5c16ad73d9abcd7d0112f52b705d39fc74
mahdi_turrkk <mahdichavoshi14@gmail.com>
2023-04-02 16:30:12 +0330

    log in page bugs fixed and layout changed

---
+++ layouts/logIn.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-app app>
+    <slot/>
+  </v-app>
+</template>
+<script setup>
+import {useTheme} from "vuetify";
+
+</script>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,14 @@
 <template>
-  <v-container fluid class="pa-0">
+  <v-container fluid class="pa-0" style="direction: rtl"
+               :style="{'background-color': useTheme().current.value.colors.background}">
     <v-row class="px-0">
       <v-col cols="0" md="6" lg="8" class="pa-0 d-none d-md-inline-block">
         <img src="~/assets/logInPageLight.jpg" class="w-100" style="object-fit: cover;height: 100.4dvh"
              alt="just a filling image"/>
       </v-col>
       <v-col cols="12" md="6" lg="4" class="pa-0" align-self="center">
-        <v-card width="80%" class="mx-auto py-8 px-3 px-md-8 elevation-5 mt-12 mt-md-0" color="background" style="border-radius: 20px">
+        <v-card width="80%" class="mx-auto py-8 px-3 px-md-8 elevation-5 mt-12 mt-md-0" color="background"
+                style="border-radius: 20px">
           <v-card-title class="text-primary text-h5 text-md-h4 text-center">گودولیون</v-card-title>
           <v-card-text class="mt-6">
             <v-form>
@@ -38,10 +40,12 @@
                         hide-details
                         v-model="password"
                         label="رمز عبور"
+                        type="password"
                     ></v-text-field>
                   </v-col>
                   <v-col cols="12">
-                    <v-btn color="primary" block style="border-radius: 10px" class="py-6 text-background text-h6">ورود</v-btn>
+                    <v-btn color="primary" block style="border-radius: 10px" class="py-6 text-background text-h6">ورود
+                    </v-btn>
                   </v-col>
                 </v-row>
               </v-container>
@@ -55,6 +59,10 @@
 
 <script setup>
 import {useTheme} from "vuetify";
+
+definePageMeta({
+  layout: "logIn",
+});
 
 let username = ref(undefined)
 let password = ref(undefined)
