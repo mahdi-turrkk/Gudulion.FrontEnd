ef7c64713dc05fa0e1c0205653807b0ae3fc2391
mahdi_turrkk <mahdichavoshi14@gmail.com>
2023-04-03 11:52:23 +0330

    notif and user menu added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,12 @@
 <template>
   <v-app app style="direction: rtl">
     <v-app-bar :elevation="0" color="primary" class="mb-16">
-      <v-btn icon class="d-inline-block d-lg-none" @click="drawer = !drawer">
-        <v-icon color="background" size="35">mdi-menu</v-icon>
+      <v-btn icon class="d-inline-block d-md-none" @click="drawer = !drawer">
+        <v-icon color="background" size="30">mdi-menu</v-icon>
       </v-btn>
       <div class="text-background text-h6 text-md-h5 d-inline-block mr-2 mr-lg-10">گودولیون</div>
       <v-spacer/>
-      <div class="d-none d-lg-inline-block">
+      <div class="d-none d-md-inline-block">
         <div class="d-inline-block">
           <nuxt-link
               to="/user"
@@ -51,13 +51,35 @@
         </div>
       </div>
       <v-spacer/>
-      <v-avatar class="ml-2 ml-lg-10">
-        <v-img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9Kd2hHYncksEm89obcsVbQoT5px2bQcSWaA&usqp=CAU"
-            style="object-fit: cover !important"
-            alt="avatar"
-        />
-      </v-avatar>
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <div class="d-inline-block" v-bind="props" style="cursor: pointer">
+            <v-icon color="background" size="30">mdi-account</v-icon>
+            <v-icon color="background">mdi-menu-down</v-icon>
+          </div>
+        </template>
+        <v-list :style="{'background-color' : useTheme().current.value.colors.background}" class="mt-1" style="direction: rtl">
+          <v-list-item>
+            <v-avatar class="d-inline-block">
+              <v-img
+                  :src="user.image"
+                  style="object-fit: cover !important"
+                  alt="avatar"
+              />
+            </v-avatar>
+            <div class="d-inline-block mr-4 text-info">{{user.name}}</div>
+          </v-list-item>
+          <v-list-item style="cursor: pointer">
+            <v-icon color="info" size="30">mdi-logout</v-icon>
+            <div class="d-inline-block mr-4 text-info">خروج از حساب کاربری</div>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+      <div class="d-inline-block ml-2 ml-lg-8 mr-4" >
+        <v-badge style="cursor: pointer" content="0" color="error">
+          <v-icon color="background" size="28">mdi-bell</v-icon>
+        </v-badge>
+      </div>
     </v-app-bar>
     <v-navigation-drawer
         app
@@ -137,7 +159,7 @@
 import {useTheme} from "vuetify";
 
 let drawer = ref(false)
-
+let user = reactive({name : "نیلوفر شاه حسینی" , image : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9Kd2hHYncksEm89obcsVbQoT5px2bQcSWaA&usqp=CAU'})
 </script>
 
 <style>
