33de4df3d865e1e32ca58e0b95c1fa5f7d4a28a3
mahdi_turrkk <mahdichavoshi14@gmail.com>
2023-04-03 10:18:22 +0330

    dash position card finished and static images of dashboard added

--- components/dashPositionCard.vue
+++ components/dashPositionCard.vue
@@ -1,9 +1,17 @@
 <template>
-  <v-container>dash Position Card</v-container>
+  <v-container fluid class="pa-0 ma-0">
+    <v-card color="background" elevation="5" class="text-center pb-10" style="border-radius: 20px">
+      <v-row class="justify-center">
+        <img :src="imageSrc" height="200" :alt="title" class="my-8" style="border-radius: 20px"/>
+      </v-row>
+      <v-card-title class="text-primary text-h5 text-md-h4">{{ title }}</v-card-title>
+      <v-card-title class="text-info text-h6 text-md-h5">{{ heading }}</v-card-title>
+    </v-card>
+  </v-container>
 </template>
 
-<script>
-export default {
-  name : "dashPositionCard"
-}
+<script setup>
+
+let props = defineProps(['imageSrc' , 'title' , 'heading'])
+
 </script>
