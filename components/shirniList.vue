ffb00e920b9bd1b1179aeda02e1341a8ca63a195
mahdi_turrkk <mahdichavoshi14@gmail.com>
2023-04-04 12:40:29 +0330

    shirni list item and shirni list component finished without routing

---
+++ components/shirniList.vue
@@ -0,0 +1,72 @@
+<template>
+    <v-container fluid class="pa-0 ma-0">
+        <v-row class="px-2">
+            <v-col cols="12" sm="6" md="5" lg="4" xl="3" class="px-0">
+                <v-text-field
+                        label="فیلتر بر اساس نام فرد"
+                        variant="underlined"
+                        color="primary"
+                        append-icon="mdi-filter"
+                        @click:append="filter"
+                        :style="{'color' : useTheme().current.value.colors.primary}"
+                ></v-text-field>
+            </v-col>
+        </v-row>
+        <v-card color="primary" elevation="5" class="py-1 px-2" style="border-radius: 20px">
+            <div class="py-2 px-4 d-none d-lg-block">
+                <div class="d-inline-block text-h6 text-md-h5 text-background" style="width: 12%">کد رهگیری</div>
+                <div class="d-inline-block text-h6 text-md-h5 text-background text-center" style="width: 20%">عنوان
+                </div>
+                <div class="d-inline-block text-h6 text-md-h5 text-background text-center" style="width: 20%">تاریخ
+                </div>
+                <div class="d-inline-block text-h6 text-md-h5 text-background text-center" style="width: 20%">نام فرد
+                </div>
+                <div class="d-inline-block text-h6 text-md-h5 text-background text-center" style="width: 16%">وضعیت
+                </div>
+                <div class="d-inline-block text-h6 text-md-h5 text-background text-center" style="width: 12%">عمل ها
+                </div>
+            </div>
+            <div class="py-2 px-4 d-none d-sm-block d-lg-none">
+                <div class="d-inline-block text-h6 text-md-h5 text-background" style="width: 33%">عنوان</div>
+                <div class="d-inline-block text-h6 text-md-h5 text-background text-center" style="width: 35%">وضعیت
+                </div>
+                <div class="d-inline-block text-h6 text-md-h5 text-background text-center" style="width: 30%">عمل ها
+                </div>
+            </div>
+            <div class="py-2 px-4 d-block d-sm-none">
+                <div class="d-inline-block text-h6 text-md-h5 text-background" style="width: 45%">عنوان</div>
+                <div class="d-inline-block text-h6 text-md-h5 text-background text-center" style="width: 50%">وضعیت
+                </div>
+            </div>
+        </v-card>
+        <v-row class="my-4 px-3" v-for="data in datas">
+            <shirni-list-item :data="data"/>
+        </v-row>
+        <v-row style="direction: ltr" class="mt-8 mb-1 px-2">
+            <v-pagination
+                    v-model="page"
+                    :length="pages"
+                    prev-icon="mdi-menu-left"
+                    next-icon="mdi-menu-right"
+                    variant="elevated"
+                    color="background"
+                    density="comfortable"
+                    active-color="primary"
+                    total-visible="5"
+            ></v-pagination>
+        </v-row>
+    </v-container>
+</template>
+
+<script setup>
+import {useTheme} from "vuetify";
+
+const props = defineProps(['datas'])
+let page = ref(1)
+let pages = ref(100)
+
+function filter() {
+//     filter function goes here
+}
+
+</script>
--- components/shirniListItem.vue
+++ components/shirniListItem.vue
@@ -1,9 +1,164 @@
 <template>
-  <v-container>shirni list item</v-container>
+    <v-container fluid class="pa-0 ma-0">
+        <v-card color="background" elevation="5" class="py-1 px-2" style="border-radius: 20px">
+            <div class="py-2 px-4 d-none d-lg-block">
+                <div class="d-inline-block text-subtitle-1 text-md-h6 text-info" style="width: 12%">
+                    {{ data.trackingCode }}
+                </div>
+                <div class="d-inline-block text-subtitle-1 text-md-h6 text-info text-center" style="width: 20%">
+                    {{ data.title }}
+                </div>
+                <div class="d-inline-block text-subtitle-1 text-md-h6 text-info text-center" style="width: 20%">
+                    {{ data.date }}
+                </div>
+                <div class="d-inline-block text-subtitle-1 text-md-h6 text-info text-center" style="width: 20%">
+                    {{ data.personName }}
+                </div>
+                <div class="d-inline-block text-subtitle-1 text-md-h6 text-info text-center" style="width: 16%">
+                    <div class="d-inline-block text-warning" v-if="data.status == 'pending for pay'">در انتظار پرداخت
+                    </div>
+                    <div class="d-inline-block text-warning" v-if="data.status == 'pending for accept'">در انتظار
+                        تایید
+                    </div>
+                    <div class="d-inline-block text-success" v-if="data.status == 'paid'">پرداخت شده</div>
+                </div>
+                <div class="d-inline-block text-subtitle-1 text-md-h6 text-info text-center" style="width: 12%">
+                    <v-btn v-bind="props" color="success" variant="text" icon="mdi-check-bold" size="30"></v-btn>
+                    <v-btn v-bind="props" color="success" variant="text" icon="mdi-credit-card" size="30"
+                           class="mx-3"></v-btn>
+                    <v-btn v-bind="props" color="success" variant="text" icon="mdi-information-variant"
+                           size="30"></v-btn>
+                </div>
+            </div>
+            <v-expansion-panels class="d-block d-lg-none" variant="accordion">
+                <v-expansion-panel class="bg-transparent d-none d-sm-block d-lg-none">
+                    <v-expansion-panel-title class="py-2 px-4">
+                        <v-row no-gutters>
+                            <v-col cols="12">
+                                <div>
+                                    <div class="d-inline-block text-subtitle-1 text-md-h6 text-info" style="width: 35%">
+                                        {{ data.title }}
+                                    </div>
+                                    <div class="d-inline-block text-subtitle-1 text-md-h6 text-info text-center"
+                                         style="width: 35%">
+                                        <div class="d-inline-block text-warning"
+                                             v-if="data.status == 'pending for pay'">در انتظار پرداخت
+                                        </div>
+                                        <div class="d-inline-block text-warning"
+                                             v-if="data.status == 'pending for accept'">در انتظار تایید
+                                        </div>
+                                        <div class="d-inline-block text-success" v-if="data.status == 'paid'">پرداخت
+                                            شده
+                                        </div>
+                                    </div>
+                                    <div class="d-inline-block text-subtitle-1 text-md-h6 text-info text-center"
+                                         style="width: 30%">
+                                        <v-btn color="success" variant="text" icon="mdi-check-bold" size="30"></v-btn>
+                                        <v-btn color="success" variant="text" icon="mdi-credit-card" size="30"
+                                               class="mx-3"></v-btn>
+                                        <v-btn color="success" variant="text" icon="mdi-information-variant"
+                                               size="30"></v-btn>
+                                    </div>
+                                </div>
+                            </v-col>
+                        </v-row>
+                    </v-expansion-panel-title>
+                    <v-expansion-panel-text>
+                        <v-row no-gutters>
+                            <v-divider color="secondary" class="border-opacity-100"/>
+                            <v-col cols="12" sm="6" class="mt-4">
+                                <div class="text-primary d-inline-block text-md-h6">کد پیگیری:</div>
+                                <div class="d-inline-block text-subtitle-1 text-md-h6 text-info">
+                                    &nbsp{{ data.trackingCode }}
+                                </div>
+                            </v-col>
+                            <v-col cols="12" sm="6" class="mt-4">
+                                <div class="text-primary d-inline-block text-md-h6">تاریخ آخرین تغییر وضعیت:</div>
+                                <div class="d-inline-block text-subtitle-1 text-md-h6 text-info text-center">
+                                    &nbsp{{ data.date }}
+                                </div>
+                            </v-col>
+                            <v-col cols="12" sm="6" class="mt-4">
+                                <div class="text-primary d-inline-block text-md-h6">نام فرد:</div>
+                                <div class="d-inline-block text-subtitle-1 text-md-h6 text-info text-center">
+                                    &nbsp{{ data.personName }}
+                                </div>
+                            </v-col>
+                        </v-row>
+                    </v-expansion-panel-text>
+                </v-expansion-panel>
+                <v-expansion-panel class="bg-transparent d-block d-sm-none">
+                    <v-expansion-panel-title class="py-2 px-4">
+                        <v-row no-gutters>
+                            <v-col cols="12">
+                                <div>
+                                    <div class="d-inline-block text-subtitle-1 text-md-h6 text-info" style="width: 45%">
+                                        {{ data.title }}
+                                    </div>
+                                    <div class="d-inline-block text-subtitle-1 text-md-h6 text-info text-center"
+                                         style="width: 55%">
+                                        <div class="d-inline-block text-warning"
+                                             v-if="data.status == 'pending for pay'">در انتظار پرداخت
+                                        </div>
+                                        <div class="d-inline-block text-warning"
+                                             v-if="data.status == 'pending for accept'">در انتظار تایید
+                                        </div>
+                                        <div class="d-inline-block text-success" v-if="data.status == 'paid'">پرداخت
+                                            شده
+                                        </div>
+                                    </div>
+                                </div>
+                            </v-col>
+                        </v-row>
+                    </v-expansion-panel-title>
+                    <v-expansion-panel-text>
+                        <v-row no-gutters>
+                            <v-divider color="secondary" class="border-opacity-100"/>
+                            <v-col cols="12" class="mt-4">
+                                <div class="text-primary d-inline-block text-md-h6">کد پیگیری:</div>
+                                <div class="d-inline-block text-subtitle-1 text-md-h6 text-info">
+                                    &nbsp{{ data.trackingCode }}
+                                </div>
+                            </v-col>
+                            <v-col cols="12" class="mt-4">
+                                <div class="text-primary d-inline-block text-md-h6">تاریخ آخرین تغییر وضعیت:</div>
+                                <div class="d-inline-block text-subtitle-1 text-md-h6 text-info text-center">
+                                    &nbsp{{ data.date }}
+                                </div>
+                            </v-col>
+                            <v-col cols="12" class="mt-4">
+                                <div class="text-primary d-inline-block text-md-h6">نام فرد:</div>
+                                <div class="d-inline-block text-subtitle-1 text-md-h6 text-info text-center">
+                                    &nbsp{{ data.personName }}
+                                </div>
+                            </v-col>
+                            <v-col cols="12" class="mt-4 d-block d-sm-none">
+                                <v-btn color="success" class="text-background mx-1">تایید</v-btn>
+                                <v-btn color="success" class="text-background mx-1">پرداخت</v-btn>
+                                <v-btn color="success" class="text-background mx-1">اطلاعات</v-btn>
+                            </v-col>
+                        </v-row>
+                    </v-expansion-panel-text>
+                </v-expansion-panel>
+            </v-expansion-panels>
+        </v-card>
+    </v-container>
 </template>
 
-<script>
-export default {
-  name : "shirniListItem"
-}
+<script setup>
+
+const props = defineProps(['data'])
+
 </script>
+
+<style>
+
+.v-expansion-panel__shadow {
+    display: none;
+}
+
+.v-expansion-panel-title--active > .v-expansion-panel-title__overlay, .v-expansion-panel-title[aria-haspopup=menu][aria-expanded=true] > .v-expansion-panel-title__overlay {
+    opacity: 0;
+}
+
+</style>
