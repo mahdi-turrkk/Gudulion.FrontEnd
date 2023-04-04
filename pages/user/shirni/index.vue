2470c7be5f9cfae5f481f9ace9ff732765448b1c
mahdi_turrkk <mahdichavoshi14@gmail.com>
2023-04-04 12:40:47 +0330

    shirni list page finished without routing

--- pages/user/shirni/index.vue
+++ pages/user/shirni/index.vue
@@ -1,9 +1,82 @@
 <template>
-  <v-container>shirni list page</v-container>
+    <v-container class="mt-16 px-lg-10" fluid>
+        <shirni-list :datas="shirni"/>
+    </v-container>
 </template>
 
-<script>
-export default {
-  name : "shirni"
-}
+<script setup>
+
+let shirni = ref([
+    {
+        trackingCode: 'TRK12345',
+        title: 'ترفیع شغلی',
+        date: '۱۲ فروردین ۱۴۰۰',
+        personName: 'محمد صادقی',
+        status: 'pending for pay'
+    },
+    {
+        trackingCode: 'TRK12345',
+        title: 'ترفیع شغلی',
+        date: '۱۲ فروردین ۱۴۰۰',
+        personName: 'محمد صادقی',
+        status: 'pending for accept'
+    },
+    {
+        trackingCode: 'TRK12345',
+        title: 'ترفیع شغلی',
+        date: '۱۲ فروردین ۱۴۰۰',
+        personName: 'محمد صادقی',
+        status: 'paid'
+    },
+    {
+        trackingCode: 'TRK12345',
+        title: 'ترفیع شغلی',
+        date: '۱۲ فروردین ۱۴۰۰',
+        personName: 'محمد صادقی',
+        status: 'pending for pay'
+    },
+    {
+        trackingCode: 'TRK12345',
+        title: 'ترفیع شغلی',
+        date: '۱۲ فروردین ۱۴۰۰',
+        personName: 'محمد صادقی',
+        status: 'pending for accept'
+    },
+    {
+        trackingCode: 'TRK12345',
+        title: 'ترفیع شغلی',
+        date: '۱۲ فروردین ۱۴۰۰',
+        personName: 'محمد صادقی',
+        status: 'paid'
+    },
+    {
+        trackingCode: 'TRK12345',
+        title: 'ترفیع شغلی',
+        date: '۱۲ فروردین ۱۴۰۰',
+        personName: 'محمد صادقی',
+        status: 'pending for pay'
+    },
+    {
+        trackingCode: 'TRK12345',
+        title: 'ترفیع شغلی',
+        date: '۱۲ فروردین ۱۴۰۰',
+        personName: 'محمد صادقی',
+        status: 'pending for accept'
+    },
+    {
+        trackingCode: 'TRK12345',
+        title: 'ترفیع شغلی',
+        date: '۱۲ فروردین ۱۴۰۰',
+        personName: 'محمد صادقی',
+        status: 'paid'
+    },
+    {
+        trackingCode: 'TRK12345',
+        title: 'ترفیع شغلی',
+        date: '۱۲ فروردین ۱۴۰۰',
+        personName: 'محمد صادقی',
+        status: 'pending for pay'
+    },
+])
+
 </script>
