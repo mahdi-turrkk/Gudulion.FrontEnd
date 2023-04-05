6b1623b0458130e4947441cfec887fda8589758d
mahdi_turrkk <mahdichavoshi14@gmail.com>
2023-04-05 13:16:47 +0330

    give shirni page finished

--- pages/user/shirni/giveShirni/index.vue
+++ pages/user/shirni/giveShirni/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <v-container>give shirni page</v-container>
+  <v-container class="mt-16 px-lg-10" fluid>
+    <give-shirni :data="{title : 'ترفیع شغلی' , name : 'شیرنی'}"/>
+  </v-container>
 </template>
 
 <script>
