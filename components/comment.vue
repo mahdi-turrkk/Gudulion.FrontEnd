3462cc7b361fc751b8e4c1559e949c5f871a05f9
mahdi_turrkk <mahdichavoshi14@gmail.com>
2023-04-08 23:59:03 +0330

    comment card component finished

--- components/comment.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <v-container>comment</v-container>
-</template>
-
-<script>
-export default {
-  name : "comment"
-}
-</script>
---
+++ components/commentCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container fluid class="pa-0 ma-0">
+      <v-list-item>
+          <v-avatar class="d-inline-block" size="large">
+              <v-img
+                      :src="data.image"
+                      style="object-fit: cover !important"
+                      alt="avatar"
+              />
+          </v-avatar>
+          <div class="d-inline-block text-subtitle-1 text-md-h6 mr-4 text-info">{{ data.name }}</div>
+      </v-list-item>
+      <div class="text-subtitle-1 text-md-h6 text-info px-9 mt-4">{{ data.message }}</div>
+  </v-container>
+</template>
+
+<script setup>
+
+const props = defineProps(['data'])
+
+</script>
+
+<style>
+.v-field--variant-solo {
+    box-shadow: none;
+}
+</style>
