28ef11f2164dd1b6ca0bd5c3a465bbb6de8ba32b
mahdi_turrkk <mahdichavoshi14@gmail.com>
2023-04-10 10:34:41 +0330

    log in page auto dark theme added and dark mode image added

--- pages/index.vue
+++ pages/index.vue
@@ -1,70 +1,80 @@
 <template>
-  <v-container fluid class="pa-0" style="direction: rtl"
-               :style="{'background-color': useTheme().current.value.colors.background}">
-    <v-row class="px-0">
-      <v-col cols="0" md="6" lg="8" class="pa-0 d-none d-md-inline-block">
-        <img src="~/assets/logInPageLight.jpg" class="w-100" style="object-fit: cover;height: 100.4dvh"
-             alt="just a filling image"/>
-      </v-col>
-      <v-col cols="12" md="6" lg="4" class="pa-0" align-self="center">
-        <v-card width="80%" class="mx-auto py-8 px-3 px-md-8 elevation-5 mt-12 mt-md-0" color="background"
-                style="border-radius: 20px">
-          <v-card-title class="text-primary text-h5 text-md-h4 text-center">گودولیون</v-card-title>
-          <v-card-text class="mt-6">
-            <v-form>
-              <v-container>
-                <v-row>
-                  <v-col
-                      cols="12"
-                  >
-                    <v-text-field
-                        color="primary"
-                        variant="solo"
-                        bg-color="transparent"
-                        style="border:1px solid;border-radius: 20px"
-                        :style="{'border-color' : useTheme().current.value.colors.primary}"
-                        hide-details
-                        v-model="username"
-                        label="نام کاربری"
-                        class="text-primary"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                      cols="12"
-                  >
-                    <v-text-field
-                        color="primary"
-                        variant="solo"
-                        bg-color="transparent"
-                        style="border:1px solid;border-radius: 20px"
-                        :style="{'border-color' : useTheme().current.value.colors.primary}"
-                        hide-details
-                        v-model="password"
-                        label="رمز عبور"
-                        class="text-primary"
-                        type="password"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12">
-                    <v-btn color="primary" block style="border-radius: 10px" class="py-6 text-background text-h6">ورود
-                    </v-btn>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-form>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+    <v-container fluid class="pa-0" style="direction: rtl"
+                 :style="{'background-color': useTheme().current.value.colors.background}">
+        <v-row class="px-0">
+            <v-col cols="0" md="6" lg="8" class="pa-0 d-none d-md-inline-block">
+                <img src="~/assets/logInPageLight.jpg" class="w-100" style="object-fit: cover;height: 100.4dvh"
+                     alt="just a filling image" v-if="!useTheme().global.current.value.dark"/>
+                <img src="~/assets/logInPageDark.jpg" class="w-100" style="object-fit: cover;height: 100.4dvh"
+                     alt="just a filling image" v-if="useTheme().global.current.value.dark"/>
+            </v-col>
+            <v-col cols="12" md="6" lg="4" class="pa-0" align-self="center">
+                <v-card width="80%" class="mx-auto py-8 px-3 px-md-8 elevation-5 mt-12 mt-md-0" color="background"
+                        style="border-radius: 20px">
+                    <v-card-title class="text-primary text-h5 text-md-h4 text-center">گودولیون</v-card-title>
+                    <v-card-text class="mt-6">
+                        <v-form>
+                            <v-container>
+                                <v-row>
+                                    <v-col
+                                            cols="12"
+                                    >
+                                        <v-text-field
+                                                color="primary"
+                                                variant="solo"
+                                                bg-color="transparent"
+                                                style="border:1px solid;border-radius: 20px"
+                                                :style="{'border-color' : useTheme().current.value.colors.primary}"
+                                                hide-details
+                                                v-model="username"
+                                                label="نام کاربری"
+                                                class="text-primary"
+                                        ></v-text-field>
+                                    </v-col>
+                                    <v-col
+                                            cols="12"
+                                    >
+                                        <v-text-field
+                                                color="primary"
+                                                variant="solo"
+                                                bg-color="transparent"
+                                                style="border:1px solid;border-radius: 20px"
+                                                :style="{'border-color' : useTheme().current.value.colors.primary}"
+                                                hide-details
+                                                v-model="password"
+                                                label="رمز عبور"
+                                                class="text-primary"
+                                                type="password"
+                                        ></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12">
+                                        <v-btn color="primary" block style="border-radius: 10px"
+                                               class="py-6 text-background text-h6">ورود
+                                        </v-btn>
+                                    </v-col>
+                                </v-row>
+                            </v-container>
+                        </v-form>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script setup>
 import {useTheme} from "vuetify";
 
-definePageMeta({
-  layout: "logIn",
-});
+definePageMeta({layout: false})
+
+const theme = useTheme()
+
+onMounted(() => {
+    let today = new Date();
+    if (today.getHours() >= 18 || today.getHours() <= 6) {
+        theme.global.name.value = 'darkTheme'
+    }
+})
 
 let username = ref(undefined)
 let password = ref(undefined)
@@ -73,6 +83,6 @@ let password = ref(undefined)
 
 <style>
 .v-field--variant-solo {
-  box-shadow: none;
+    box-shadow: none;
 }
 </style>
